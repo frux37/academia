583fae6dc5ed5d2fc08c9e505075e7befa60a3cc
Charles <cacaaguierw@gmail.com>
2024-07-29 06:32:27 -0300

    criado mais uma instancia de aluno, uma de avaliador, adicionado a avaliação fisica no arrayList. Mostra as informações no final

--- src/teste/TesteCadastro.java
+++ src/teste/TesteCadastro.java
@@ -19,8 +19,13 @@ public class TesteCadastro {
         a1.setCPF("030.540.123-31");
         //a1.setDataNascimento("05/09/2001");
         
+        Aluno a2 = new Aluno("Carlos", 123456);
+        a2.setNome("Carlos Augusto Segala Aguirre");
+        a2.setCPF("030.549.710.31");
+        
         Professor prof1 = new Professor("Dr. Pauno Cuzy", 150.00);
         Professor prof2 = new Professor("Dr. Neaderthal", 200.00);
+        Professor prof3 = new Professor ("Dr. Manhattan", 300.00);
         
         AvaliacaoFisica af1 = new AvaliacaoFisica(a1, prof1);
         af1.setPeso(70);
@@ -32,14 +37,22 @@ public class TesteCadastro {
         af2.setAltura(1.75);
         af2.setPercGordura(20.0);
         
+        AvaliacaoFisica af3 = new AvaliacaoFisica(a2, prof3);
+        af3.setPeso(90);
+        af3.setAltura(1.90);
+        af3.setPercGordura(23.0);
+        
         a1.addAvaliacaoFisica(af1);
         a1.addAvaliacaoFisica(af2);
+        a2.addAvaliacaoFisica(af3);
         
         System.out.println(a1.mostraInfo());
+        System.out.println(a2.mostraInfo());
         
-        //System.out.println("\nAvaliacoes Fisicas");
-        //System.out.println(af1.mostraAvaliacao());
-        //System.out.println(af2.mostraAvaliacao());
+        System.out.println("\nAvaliacoes Fisicas");
+        System.out.println(af1.mostraAvaliacao());
+        System.out.println(af2.mostraAvaliacao());
+        System.out.println(af3.mostraAvaliacao());
         
     }
     
